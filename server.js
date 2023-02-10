7d1abcba6f725e571fce904e501be5c819ef6c0e
bfrazier <bthehippie2@gmail.com>
2023-02-09 22:51:20 -0500

    configure session

--- server.js
+++ server.js
@@ -12,3 +12,21 @@ session.Store;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+// configuration for the session
+const sess = {
+  secret: 'pinky promise',
+  cookie: {
+    maxAge: 30000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new sequelizeStore({
+    db: sequelize,
+  }),
+};
