2804a280cdbed30c8eaa3a24842e2c4c028ef9db
bfrazier <bthehippie2@gmail.com>
2023-02-09 23:26:16 -0500

    add routes; home & api

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+// gets router from express
+const router = require('express').Router();
+
+// declare path names for routes
+const apiRoutes = require('./api');
+const homeRoute = require('./homeRoute');
+
+// uses the path
+router.use('/', homeRoute);
+router.use('/ap', apiRoutes);
+
+// exports the routes
+module.exports = router;
