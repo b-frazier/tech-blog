19e3ccd6fc4801e222fb4e98d35040c566213cda
bfrazier <bthehippie2@gmail.com>
2023-02-10 00:26:09 -0500

    write delete by id route

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -14,3 +14,27 @@ router.post('/', withAuth, async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogPostData) {
+      res.status(404).json({
+        message: 'A post with this id does not exist!',
+      });
+      return;
+    }
+
+    res.status(200).json(blogPostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
