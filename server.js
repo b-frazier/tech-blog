8901692254a3f297b26c03b8f9d3a08e6378755f
bfrazier <bthehippie2@gmail.com>
2023-02-09 22:43:57 -0500

    add ; to lines. assign express. declare port.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // packages
-const express = require('express')
-const session = require('express-session')
-const exphbs = require('express-handlebars')
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 // files
-const helpers = require('./utils/helpers')
-const routes = require('./controllers')
+const helpers = require('./utils/helpers');
+const routes = require('./controllers');
 // sequelize connections
-const sequelize = require('./config/connection')
-const sequelizeStore = require('connect-session-sequelize')
-(session.Store)
+const sequelize = require('./config/connection');
+const sequelizeStore = require('connect-session-sequelize');
+session.Store;
 
+const app = express();
+const PORT = process.env.PORT || 3001;
