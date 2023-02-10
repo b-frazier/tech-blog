f54a131ace8190aa4f0d294235ee5a510bc738a0
bfrazier <bthehippie2@gmail.com>
2023-02-09 23:35:39 -0500

    write login authorization

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// if user isn't logged in, redirect to log in
+const withAuth = (req, res, next) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
