d2b01340b21b77dd490e1ae6562ec0cbd159f8b0
bfrazier <bthehippie2@gmail.com>
2023-02-10 22:56:41 -0500

    get all posts, render to page

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,3 +1,28 @@
 const router = require('express').Router();
 const { BlogPost, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+// this will get all of the posts w/ user data and render to homepage
+router.get('/', async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+    // serialize the data
+    const blogPosts = blogPostData.map((blogPost) =>
+      blogPost.get({ plain: true })
+    );
+    // pass the data into handlebars
+    res.render('homepage', {
+      blogPosts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
