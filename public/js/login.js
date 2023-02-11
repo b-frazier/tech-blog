93a67f3e37654063dfd4ed7f8be3ad2eab323b24
bfrazier <bthehippie2@gmail.com>
2023-02-10 23:44:30 -0500

    handle sign up form submission and response

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
+const { join } = require('path');
+
 const loginForm = async (e) => {
   e.preventDefault();
 
@@ -13,6 +15,28 @@ const loginForm = async (e) => {
       headers: { 'Content-Type': 'application/json' },
     });
     // if response is okay, redirect to profile page
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signUpForm = async (e) => {
+  e.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
     if (response.ok) {
       document.location.replace('/profile');
     } else {
